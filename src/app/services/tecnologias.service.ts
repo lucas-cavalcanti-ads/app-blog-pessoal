15c1f8667a6910012f921f6feac68d405e43216b
Lucas de Paula <lucas.tnv27@gmail.com>
2021-07-13 21:37:32 -0300

    SERVICE TECNOLOGIA FINALIZADA

--- src/app/services/tecnologias.service.ts
+++ src/app/services/tecnologias.service.ts
@@ -16,20 +16,18 @@ export class TecnologiasService{
     }
 
     buscarTecnologiasFrontEnd(): Array<String> {
-        return this.listaTecnologiasFrontEnd;
+        return this.listaTecnologiasFrontEnd = ["Angular 2+", "Angular JS", "Type Script", "Java Script", "Bootstrap", "HTML 5", "CSS 3"];
     }
 
     buscarTecnologiasFerramentas(): Array<String> {
-        return this.listaTecnologiasFerramentas;
+        return this.listaTecnologiasFerramentas = ["Jira Corp", "Gitlab", "Github", "Postman", "Swagger", "Adobe XD", "Android Studio"];
     }
 
     buscarTecnologiasMobile(): Array<String> {
-        return this.listaTecnologiasMobile;
+        return this.listaTecnologiasMobile = ["React Native", "Kotlin"];
     }
 
     buscarTecnologiasMetodologias(): Array<String> {
-        return this.listaTecnologiasMetodologias;
+        return this.listaTecnologiasMetodologias = ["Rest", "JSON", "Metodologia Ágil (Scrum, Kanban)", "UX/UI"];
     }
-
-
 }
