c93a11424c4d1dbeba37bd96c100e5196450daca
lucas-cavalcanti-ads <lucas.tnv27@gmail.com>
2023-06-11 18:45:24 -0300

    fix(portifolio): ajuste href botao saiba mais

--- src/app/services/portifolio.service.ts
+++ src/app/services/portifolio.service.ts
@@ -1,4 +1,5 @@
 import { Portifolio } from './../models/portifolio.model';
+
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
@@ -16,7 +17,7 @@ export class PortifoliosService{
             },
             {
                 title: "Projeto Hotel",
-                description: "Essa foi a primeira Land Page que la em 2019, criei utilizando com HTML 5 e CSS 3.",
+                description: "Minha primeira Land Page criada em 2019, criei utilizando com HTML5 e CSS3.",
                 link: "https://github.com/lucas-cavalcanti-ads/projeto-interface-hotel",
                 infos_chaves: ["html", "css"]
             },
@@ -28,7 +29,7 @@ export class PortifoliosService{
             },
             {
                 title: "Artigo sobre Github Actions",
-                description: "Artigo sobre Github Actions publicado dentro da página do Itaú no Medium. O artigo fala sobre o que é e como funciona a poderosa ferramenta do Github Actions.",
+                description: "Artigo sobre Github Actions publicado dentro da página do Itaú no Medium.",
                 link: "https://medium.com/itautech/github-actions-boas-pr%C3%A1ticas-para-a-automatiza%C3%A7%C3%A3o-de-pipelines-de-entrega-2d142f5aea2f",
                 infos_chaves: ["github"]
             }
